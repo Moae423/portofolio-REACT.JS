4e353aa630eb851871257c276bb17d1b4ce02d09
Daffa Rihadatul Fairus <hanura423@gmail.com>
2024-12-23 20:58:50 +0700

    edit hero section

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -11,40 +11,39 @@ export default function HeroSection() {
   return (
     <section
       style={{ backgroundImage: `url(${gambar})` }}
-      className="bg-center min-h-screen bg-no-repeat bg-cover bg-gray-700 bg-blend-multiply"
+      className="min-h-screen  bg-cover bg-gray-700 bg-blend-soft-light flex items-center justify-center"
     >
-      <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+      <div className="px-4 mx-auto  max-w-screen-xl text-center ">
         <h1
           id="YourName"
-          className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-6xl lg:text-6xl"
+          className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl"
         >
           {/* namanya user akan muncul disini ketika di input dari button dibawah */}
           Say Your Name!
         </h1>
-        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-3xl md:text-2xl sm:text-xl sm:px-16 lg:px-48">
           Selamat datang di website gw, sebelum scroll kebawah, yuk ketik namamu
           disini supaya kita saling kenal nantinya😁
         </p>
         {/* input */}
-        <div className="flex items-center max-w-sm mx-auto">
+        <div className="flex items-center max-w-md mx-auto">
           <div className="relative w-full">
             <input
               type="text"
               id="inputName"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Ketik Dong Namanya Disini"
               required=""
             />
+            {/* input end*/}
           </div>
           <button
             onClick={handleClick}
-            className="ms-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="ms-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm py-2 px-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            Klik Disini!
+            V
           </button>
         </div>
-
-        {/* input end*/}
       </div>
     </section>
   );
